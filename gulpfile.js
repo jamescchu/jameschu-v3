a24440806ace688ea5d391e0a33f5760e0ecf607
James Chu <james@james-chu.com>
2016-10-18 00:17:46 -0700

    Added Bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync'),
-    clean = require('gulp-clean'),
+    mainBowerFiles = require('main-bower-files'),
     Hexo = require('hexo'),
     hexo = new Hexo(process.cwd(), {});
 
+var plugins = require("gulp-load-plugins")({
+    pattern: ['gulp-*', 'gulp.*'],
+    replaceString: /\bgulp[\-.]/
+});
+
+var dest = 'themes/jameschu/source/';
+
+// Generate Hexo Files
 gulp.task('hexo-gen', function() {
     hexo.init().then(function() {
         return hexo.call('generate', {
@@ -14,6 +22,7 @@ gulp.task('hexo-gen', function() {
     });
 });
 
+// Start BrowserSync with Generation
 gulp.task('browser-sync', ['hexo-gen'], function() {
     browserSync({
         open: false,
@@ -25,19 +34,23 @@ gulp.task('browser-sync', ['hexo-gen'], function() {
     });
 });
 
-gulp.task('hexo-reload', ['hexo-gen'], function() {
-    browserSync.reload();
-});
-
-gulp.task('watch', function() {
-    gulp.watch('public/*', ['hexo-reload']);
-});
-
+// Clean Generated Folder
 gulp.task('clean', function() {
     return gulp.src('public', {
-            read: false
-        })
-        .pipe(clean());
+        read: false
+    }).pipe(plugins.clean());
 });
 
-gulp.task('default', ['clean', 'browser-sync', 'hexo-gen']);
+// Move Bower Files
+gulp.task('bower-css', function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(plugins.filter('*.css'))
+        .pipe(gulp.dest(dest + 'stylesheets/vendor'));
+});
+
+// Watch for Updates
+gulp.task('watch', function() {
+    gulp.watch('bower_components/*', ['bower-css']);
+});
+
+gulp.task('default', ['clean', 'browser-sync', 'watch']);
