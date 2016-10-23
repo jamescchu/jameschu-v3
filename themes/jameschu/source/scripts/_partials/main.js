37cfe75d1a87a2cdc5cdcfd1589aa1d7a30f703c
James Chu <james@james-chu.com>
2016-10-22 17:13:54 -0700

    implemented page transitions

--- themes/jameschu/source/scripts/_partials/main.js
+++ themes/jameschu/source/scripts/_partials/main.js
@@ -2,24 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     var dom = Barba.Pjax.Dom;
     dom.wrapperId = 'content-wrapper';
     dom.containerClass = 'content-container';
-    
+
     Barba.Pjax.init();
     Barba.Prefetch.init();
 
-    var nav = document.querySelector('.nav-list');
-    var items = menu.querySelectorAll('.nav-list-link');
-    var wrapper = document.getElementById('content-wrapper');
-
-    // Add the active class when click on the menu item.
-    Barba.Dispatcher.on('linkClicked', function(el) {
-        if (!menu.contains(el)) {
-            return;
-        }
-
-        [].forEach.call(items, function(item) {
-            item.classList.remove('active');
-        });
-
-        el.parentElement.classList.add('active');
+    // Scroll to the wrapper
+    Barba.Dispatcher.on('transitionCompleted', function() {
+        window.scrollTo(0, wrapper.getBoundingClientRect().top);
     });
+
+    // Add transition to barba
+    Barba.Pjax.getTransition = function() {
+      return Transition;
+    };
+
+    Barba.transitionLength = 250;
 });
--- themes/jameschu/source/scripts/global.js
+++ themes/jameschu/source/scripts/global.js
@@ -1708,28 +1708,114 @@ return /******/ (function(modules) { // webpackBootstrap
 });
 ;
 //# sourceMappingURL=barba.js.map
+// Define barba properties
+Barba.transitionLength = 500;
+
+// Define transition
+var Transition = Barba.BaseTransition.extend({
+  start: function() {
+    this.newContainerLoading.then(this.runTransition.bind(this));
+  },
+
+  runTransition: function() {
+
+    document.body.style.overflow = 'hidden';
+
+    ////////////////////////////
+    // Setup
+    ////////////////////////////
+    var transitionLength = parseInt(Barba.transitionLength),
+        transitionTimeout = 100,
+        transitionLengthSeconds = (transitionLength / 1000) + 's',
+        transitionSelector = 'data-transition'; 
+    ////////////////////////////
+    
+    // Set the animation time on all elements
+    var allAnimationElements = document.querySelectorAll('[' + transitionSelector + ']');
+    for (var i = 0; i < allAnimationElements.length; i++) {
+      element = allAnimationElements[i];
+
+      // Set styles
+      element.style.animationDuration = transitionLengthSeconds;
+      element.style.animationDelay = transitionLengthSeconds;
+      element.style.animationName = element.dataset.transition;
+      element.style.animationFillMode = 'forwards';
+    }
+
+    // Get all old elements with transitions
+    var oldElements = this.oldContainer.querySelectorAll('[' + transitionSelector + ']');
+    for (var i = 0; i < oldElements.length; i++) {
+      element = oldElements[i];
+
+      // Remove style tag
+      element.removeAttribute('style');
+    }
+    
+    // Trigger out transitions
+    setTimeout(function(){
+
+      for (var i = 0; i < oldElements.length; i++) {
+        element = oldElements[i];
+
+        // Set styles
+        element.style.animationDuration = transitionLengthSeconds;
+        element.style.animationDelay = '0s';
+        element.style.animationName = element.dataset.transition;
+        element.style.animationFillMode = 'forwards';
+        element.style.animationDirection = 'alternate-reverse';
+      }
+
+    }, transitionTimeout);
+
+    var x = this;
+    function done(x) {
+
+      // Remove old container and add new one
+      x.oldContainer.style.visibility = 'hidden';
+      x.newContainer.style.visibility = 'visible';
+
+      // Remove style tag at the end of the animation
+      setTimeout(function(){
+        document.body.style.overflow = 'visible';
+        for (var i = 0; i < allAnimationElements.length; i++) {
+          element = allAnimationElements[i];
+          element.removeAttribute('style');
+        }
+      }, transitionLength);
+
+      // Scroll to top
+      document.body.scrollTop = 0;
+
+      // Done
+      x.done();
+
+    }
+
+    // Mark as done
+    setTimeout(function(){
+      done(x);
+    }, transitionLength + transitionTimeout);
+
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     var dom = Barba.Pjax.Dom;
     dom.wrapperId = 'content-wrapper';
     dom.containerClass = 'content-container';
-    
+
     Barba.Pjax.init();
     Barba.Prefetch.init();
 
-    var nav = document.querySelector('.nav-list');
-    var items = menu.querySelectorAll('.nav-list-link');
-    var wrapper = document.getElementById('content-wrapper');
-
-    // Add the active class when click on the menu item.
-    Barba.Dispatcher.on('linkClicked', function(el) {
-        if (!menu.contains(el)) {
-            return;
-        }
-
-        [].forEach.call(items, function(item) {
-            item.classList.remove('active');
-        });
-
-        el.parentElement.classList.add('active');
+    // Scroll to the wrapper
+    Barba.Dispatcher.on('transitionCompleted', function() {
+        window.scrollTo(0, wrapper.getBoundingClientRect().top);
     });
+
+    // Add transition to barba
+    Barba.Pjax.getTransition = function() {
+      return Transition;
+    };
+
+    Barba.transitionLength = 250;
 });
